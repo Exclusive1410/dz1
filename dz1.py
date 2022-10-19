7d172248f5a643798ca9e48c0e55aa9f11844279
Exclusive1410 <72496207+Exclusive1410@users.noreply.github.com>
2022-10-19 13:00:25 +0300

    Add files via upload

---
+++ dz1.py
@@ -0,0 +1,17 @@
+class product:
+    def __init__(self, keyboard, price):
+        if keyboard <= 0:
+            raise ValueError('You didnt order anything')
+        self.keyboard = keyboard
+        self.price = price
+
+def _add_price(self):
+    return self.price - (price/100*20)
+
+def __get_price(self):
+    if self.__add_price(self) == 10:
+        return self.keyboard - 1
+    return self.keyboard
+
+def __str__(self):
+    return f'Price of products: ${self.__get_price()}'
